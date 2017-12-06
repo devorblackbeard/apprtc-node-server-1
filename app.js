54e67709e24040a6363fe2800ea8d99800a717e7
nazar.zhybulski@gmail.com <nazar.zhybulski@gmail.com>
2017-12-06 12:38:36 +0200

    init commit

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const router 	   = express.Router();
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+require('./routes')(router);
 
 var app = express();
 
@@ -27,6 +29,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+require('./routes')(router);
+app.use('/api/v1', router);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -40,7 +45,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
+    res.status(err.status || 200);
     res.render('error', {
       message: err.message,
       error: err
@@ -51,8 +56,9 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
+  res.status(err.status || 200);
+ res.
+  render('error', {
     message: err.message,
     error: {}
   });
